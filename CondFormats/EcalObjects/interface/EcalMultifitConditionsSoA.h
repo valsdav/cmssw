37d66446af7e171b614a3dd2d53fd1effd6c7f88
Davide Valsecchi <davide.valsecchi@cern.ch>
2023-02-22 14:42:48 +0100

    Completed SOA for multifit conditions
    Completed SoA for multifit conditions

--- CondFormats/EcalObjects/interface/EcalMultifitConditionsSoA.h
+++ CondFormats/EcalObjects/interface/EcalMultifitConditionsSoA.h
@@ -10,10 +10,14 @@
 
 
 using PulseShapeArray = std::array<float, EcalPulseShape::TEMPLATESAMPLES>;
-using SampleCorrelationArray = std::array<float, ecalPh1::sampleSize>;
+using SampleCorrelationArray = std::array<double, ecalPh1::sampleSize>;
 
 using CovarianceMatrix = Eigen::Matrix<float, EcalPulseShape::TEMPLATESAMPLES, EcalPulseShape::TEMPLATESAMPLES>;
 
+constexpr size_t N_TIMEBIASCORRECTIONS_BINS_EB = 71;
+constexpr size_t N_TIMEBIASCORRECTIONS_BINS_EE = 58;
+using TimeBiasCorrArrayEB = std::array<float, N_TIMEBIASCORRECTIONS_BINS_EB>;
+using TimeBiasCorrArrayEE = std::array<float, N_TIMEBIASCORRECTIONS_BINS_EE>;
 
 
 GENERATE_SOA_LAYOUT(EcalMultifitConditionsSoALayout,
@@ -24,18 +28,29 @@ GENERATE_SOA_LAYOUT(EcalMultifitConditionsSoALayout,
                     SOA_COLUMN(float, pedestals_rms_x12),
                     SOA_COLUMN(float, pedestals_rms_x6),
                     SOA_COLUMN(float, pedestals_rms_x1),
-                    SOA_COLUMN(PulseShapeArray, pulse_shapes),
-                    SOA_EIGEN_COLUMN(CovarianceMatrix, pulse_covariance),
-                    SOA_COLUMN(float, gainRatios_gain12Over6),
-                    SOA_COLUMN(float, gainRatios_gain6Over1),
+                    SOA_COLUMN(PulseShapeArray, pulseShapes),
+                    // NxN  N=templatesamples  for each xtal
+                    SOA_EIGEN_COLUMN(CovarianceMatrix, pulseCovariance),
+                    SOA_COLUMN(float, gain12Over6),
+                    SOA_COLUMN(float, gain6Over1),
+                    SOA_COLUMN(float, timeCalibConstants),
+                    // timeBiasCorrections (fixed since 2011)
+                    SOA_SCALAR(TimeBiasCorrArrayEB, timeBiasCorrections_amplitude_EB),
+                    SOA_SCALAR(TimeBiasCorrArrayEB, timeBiasCorrections_shift_EB),
+                    SOA_SCALAR(TimeBiasCorrArrayEE, timeBiasCorrections_amplitude_EE),
+                    SOA_SCALAR(TimeBiasCorrArrayEE, timeBiasCorrections_shift_EE),
                     // Sample correlation scalar: array of 10 values for each gain in EB and EE
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EB_G12),
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EB_G6),
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EB_G1),
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EE_G12),
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EE_G6),
-                    SOA_SCALAR(SampleCorrelationArray, sample_correlation_EE_G1)
-
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EB_G12),
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EB_G6),
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EB_G1),
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EE_G12),
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EE_G6),
+                    SOA_SCALAR(SampleCorrelationArray, sampleCorrelation_EE_G1),
+                    // Samples Masks
+                    SOA_SCALAR(unsigned int, sampleMask_EB),
+                    SOA_SCALAR(unsigned int, sampleMask_EE),
+                    SOA_SCALAR(float, timeOffset_EB),
+                    SOA_SCALAR(float, timeOffset_EE)
   )
 
 using EcalMultifitConditionsSoA = EcalMultifitConditionsSoALayout<>;
