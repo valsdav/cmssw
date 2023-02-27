23f48d042ae6f189318378781032c1a12e3ae1e6
Davide Valsecchi <davide.valsecchi@cern.ch>
2023-02-27 10:38:39 +0100

    Improved options struct for TF session

--- PhysicsTools/TensorFlow/test/testConstSessionCUDA.cc
+++ PhysicsTools/TensorFlow/test/testConstSessionCUDA.cc
@@ -56,12 +56,12 @@ process.add_(cms.Service('CUDAService'))
   tensorflow::Backend backend = tensorflow::Backend::cuda;
 
   // load the graph
-  tensorflow::setLogging();
+  tensorflow::Options options{backend};
   tensorflow::GraphDef* graphDef = tensorflow::loadGraphDef(pbFile);
   CPPUNIT_ASSERT(graphDef != nullptr);
 
   // create a new session and add the graphDef
-  const tensorflow::Session* session = tensorflow::createSession(graphDef, backend);
+  const tensorflow::Session* session = tensorflow::createSession(graphDef, options);
   CPPUNIT_ASSERT(session != nullptr);
 
   // example evaluation
--- PhysicsTools/TensorFlow/test/testSessionCacheCUDA.cc
+++ PhysicsTools/TensorFlow/test/testSessionCacheCUDA.cc
@@ -53,11 +53,10 @@ process.add_(cms.Service('CUDAService'))
 
   std::cout << "Testing CUDA backend" << std::endl;
   tensorflow::Backend backend = tensorflow::Backend::cuda;
-
-  tensorflow::setLogging();
+  tensorflow::Options options{backend};
 
   // load the graph and the session
-  tensorflow::SessionCache cache(pbFile, backend);
+  tensorflow::SessionCache cache(pbFile, options);
   CPPUNIT_ASSERT(cache.graph.load() != nullptr);
   CPPUNIT_ASSERT(cache.session.load() != nullptr);
 
