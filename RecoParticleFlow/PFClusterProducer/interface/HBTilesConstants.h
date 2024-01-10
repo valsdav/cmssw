93440ad8eae2988f3f15f8eb3b1da6ebaf0124c9
brusale <brusale11@gmail.com>
2024-01-10 14:13:28 +0100

    introduce BarrelLayerTiles

---
+++ RecoParticleFlow/PFClusterProducer/interface/HBTilesConstants.h
@@ -0,0 +1,28 @@
+// Authors: Felice Pantaleo - felice.pantaleo@cern.ch
+// Date: 03/2019
+
+#ifndef RecoLocalCalo_HGCalRecProducer_HBTilesConstants_h
+#define RecoLocalCalo_HGCalRecProducer_HBTilesConstants_h
+
+#include "DataFormats/Math/interface/constexpr_cmath.h"
+#include <cmath>
+#include <cstdint>
+#include <array>
+
+struct HBTilesConstants {
+
+  static constexpr float tileSizeEtaPhi = 3*0.087f;
+  static constexpr float minDim1 = -1.5f;
+  static constexpr float maxDim1 = 1.5f;
+  static constexpr float minDim2 = -M_PI;
+  static constexpr float maxDim2 = M_PI;
+  static constexpr int nColumns = reco::ceil((maxDim1 - minDim1) / tileSizeEtaPhi);
+  static constexpr int nRows = reco::ceil(2. * M_PI / tileSizeEtaPhi);
+  static constexpr int nTiles = nColumns * nRows;
+  static constexpr float cellWidthEta = 3*0.087f;
+  static constexpr float cellWidthPhi = 3*0.087f;
+  static constexpr float showerSigma = 0.5f; // FIXME // in unit of xtals
+
+};
+
+#endif
