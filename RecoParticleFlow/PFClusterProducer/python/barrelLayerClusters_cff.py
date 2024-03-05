5ea3156b8e5544ab4f1aec9829465443c4925df2
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-03-05 15:19:18 +0100

    Added defaults for EB Clue

--- RecoParticleFlow/PFClusterProducer/python/barrelLayerClusters_cff.py
+++ RecoParticleFlow/PFClusterProducer/python/barrelLayerClusters_cff.py
@@ -2,6 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoParticleFlow.PFClusterProducer.barrelLayerClusters_cfi import barrelLayerClusters
 
+
+# ECAL
+barrelLayerClusters.ebplugin.kappa = cms.double(3.5)
+barrelLayerClusters.ebplugin.deltac = cms.vdouble(1.8*0.0175, 5*0.087, 5*0.087)
+barrelLayerClusters.ebplugin.outlierDeltaFactor = cms.double(2.7*0.0175)
+barrelLayerClusters.ebplugin.maxLayerIndex = cms.int32(0)
+#barrelLayerClusters.ebplugin.fractionCutoff = cms.double(0.01)
+
 # HCAL
 barrelLayerClusters.hbplugin.kappa = cms.double(1)
 barrelLayerClusters.hbplugin.deltac = cms.vdouble(1.8 * 0.0175, 3 * 0.087, 3 * 0.087)
