3898c1c5dd96ff8965d46a1a46661fd2a42bda5b
Christine Zeh <zeh.chrisi@gmail.com>
2025-03-10 11:39:20 +0100

    add function to create tensor with stride automatically

--- PhysicsTools/PyTorch/test/testTorchFromBlobStride.cu
+++ PhysicsTools/PyTorch/test/testTorchFromBlobStride.cu
@@ -14,77 +14,108 @@ using std::chrono::duration_cast;
 using std::chrono::duration;
 using std::chrono::milliseconds;
 
+template <std::size_t N>
+torch::Tensor array_to_tensor(torch::Device device, int* arr, const long int* size) {
+  long int arr_size[N];
+  long int arr_stride[N];
+  std::copy(size, size+N, arr_size);
+  std::copy(size, size+N, arr_stride);
+
+  std::reverse(std::begin(arr_size), std::end(arr_size));
+  arr_stride[N-1] = 1;
+  arr_stride[0] *= arr_stride[1];
+
+  auto options = torch::TensorOptions().dtype(torch::kInt).device(device).pinned_memory(true);
+  torch::Tensor tensor = torch::from_blob(arr, arr_size, arr_stride, options);
+
+  return tensor;
+}
+
+template <std::size_t N>
+void print_column_major(int* arr, const long int* size) {
+  if (N == 2) {
+    for (int j = 0; j < size[0]; j++) {
+      for (int i = 0; i < size[1]; i++) {
+          std::cout << arr[i + j*size[1]] << " ";
+      }
+      std::cout << std::endl;
+    } 
+  } else if (N == 3) {
+    for (int k = 0; k < size[N-1]; k++) {
+      std::cout << "(" << k << ")..." << std::endl;
+      for (int j = 0; j < size[N-2]; j++) {
+          for (int i = 0; i < size[0]; i++) {
+            std::cout << arr[i + j*size[0] + k*size[0]*size[1]] << " ";
+          }
+          std::cout << std::endl;
+      }
+      std::cout << std::endl;
+    }
+}
+}
+
+
 int main(int argc, char* argv[]) {  
   torch::Device device(torch::kCUDA);
 
   int a_cpu[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-  long int a_dim_cpu[] = {3, 6};
+  // 2 Dimensional Example
+  // long int a_dim_cpu[] = {3, 6};
+
+  // 3 Dimensional Example
+  const long int a_dim_cpu[] = {3, 3, 2};
 
   const size_t dims = sizeof(a_dim_cpu) / sizeof(long int);
-  long int a_size_cpu[dims];
-  long int a_stride_cpu[dims];
-  std::copy(std::begin(a_dim_cpu), std::end(a_dim_cpu), a_size_cpu);
-  std::copy(std::begin(a_dim_cpu), std::end(a_dim_cpu), a_stride_cpu);
-
-  std::reverse(std::begin(a_size_cpu), std::end(a_size_cpu));
-  a_stride_cpu[0] = 1;
-
-  for (int i = 0; i < a_dim_cpu[0]; i++) {
-    for (int j = 0; j < a_dim_cpu[1]; j++) {
-        std::cout << a_cpu[i*a_dim_cpu[1] + j] << " ";
-    }
-    std::cout << std::endl;
-  } 
+  print_column_major<dims>(a_cpu, a_dim_cpu);
 
   int *a_gpu;
   cudaMalloc(&a_gpu, sizeof(a_cpu));
   cudaMemcpy(a_gpu, a_cpu, sizeof(a_cpu), cudaMemcpyHostToDevice);
 
+  // bad behaviour
   auto options = torch::TensorOptions().dtype(torch::kInt).device(device).pinned_memory(true);
   std::cout << "Converting vector to Torch tensors on CPU without stride" << std::endl;
   torch::Tensor a_tensor = torch::from_blob(a_gpu, a_dim_cpu, options);
+  std::cout << a_tensor << std::endl;  
+
+  // Correct Transposition to get to column major. Still wrong.
+  std::cout << "Transpose Tensor to get same dimensions" << std::endl;
+  a_tensor = torch::transpose(a_tensor, 0, dims-1);
+  a_tensor = torch::transpose(a_tensor, 1, dims-1);
   std::cout << a_tensor << std::endl;
 
-  std::cout << "Transpose Tensor" << std::endl;
-  std::cout << torch::transpose(a_tensor, 0, 1) << std::endl;
 
+  // Use stride to read correctly.
   std::cout << "Converting vector to Torch tensors on CPU with stride" << std::endl;
-  torch::Tensor b_tensor = torch::from_blob(a_gpu, a_size_cpu, a_stride_cpu, options);
-  std::cout << b_tensor << std::endl;
+  std::cout << array_to_tensor<dims>(device, a_gpu, a_dim_cpu) << std::endl;
 
-  long int b_dim[] = {500, 1000};
-  int b[b_dim[0]][b_dim[1]];
+  long int size_b[] = {500, 1000};
+  int b[size_b[0]][size_b[1]];
 
-
-  for (int i = 0; i < b_dim[0]; i++) {
-    for (int j = 0; j < b_dim[1]; j++) {
+  for (int i = 0; i < size_b[0]; i++) {
+    for (int j = 0; j < size_b[1]; j++) {
       b[i][j] = rand();
     }
   } 
 
   int *b_gpu;
-  cudaMalloc(&b_gpu, b_dim[0] * b_dim[1] * sizeof(int));
-  cudaMemcpy(b_gpu, b, b_dim[0] * b_dim[1] * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMalloc(&b_gpu, size_b[0] * size_b[1] * sizeof(int));
+  cudaMemcpy(b_gpu, b, size_b[0] * size_b[1] * sizeof(int), cudaMemcpyHostToDevice);
 
   std::cout << "Benchmark stride and transpose:" << std::endl;
 
   auto t1 = high_resolution_clock::now();
-  long int b_size[2];
-  long int b_stride[2];
-  std::copy(std::begin(b_dim), std::end(b_dim), b_size);
-  std::copy(std::begin(b_dim), std::end(b_dim), b_stride);
-
-  std::reverse(std::begin(b_size), std::end(b_size));
-  b_stride[0] = 1;
-
-  torch::Tensor d_tensor = torch::from_blob(b_gpu, b_size, b_stride, options);
+  const size_t dim_b = sizeof(size_b) / sizeof(long int);
+  array_to_tensor<dim_b>(device, b_gpu, a_dim_cpu);
   auto t2 = high_resolution_clock::now();
   duration<double, std::milli> ms_double = t2 - t1;
   std::cout << "Stride:" << ms_double.count() << "ms\n";
 
   t1 = high_resolution_clock::now();
-  torch::Tensor e_tensor = torch::from_blob(b_gpu, b_dim, options);
-  e_tensor = torch::transpose(e_tensor, 0, 1);
+  const size_t dim_b2 = sizeof(size_b) / sizeof(long int);
+  torch::Tensor e_tensor = torch::from_blob(b_gpu, dim_b2, options);
+  a_tensor = torch::transpose(a_tensor, 0, dim_b2-1);
+  a_tensor = torch::transpose(a_tensor, 1, dim_b2-1);
   t2 = high_resolution_clock::now();
   ms_double = t2 - t1;
   std::cout << "Transpose:" << ms_double.count() << "ms\n";
