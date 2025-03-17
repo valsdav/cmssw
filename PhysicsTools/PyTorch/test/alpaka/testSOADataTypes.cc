94364548f28d8de479bc79b1bf25e402d7280a31
Christine Zeh <zeh.chrisi@gmail.com>
2025-03-17 11:49:39 +0100

    move datatype test to gpu

--- PhysicsTools/PyTorch/test/alpaka/testSOADataTypes.cc
+++ PhysicsTools/PyTorch/test/alpaka/testSOADataTypes.cc
@@ -160,8 +160,30 @@ torch::Tensor Converter<SOA_Layout>::convert_single(int nElements, MaskElement m
 
 
 void testSOADataTypes::test_pose_SOA() {
-  std::cout << "SOA with multiple bunches filled and 7 rows." << std::endl;
-  torch::Device device(torch::kCPU);
+  std::cout << "ALPAKA Platform info:" << std::endl;
+  int idx = 0;
+  try {
+    for (;;) {
+      alpaka::Platform<alpaka::DevCpu> platformHost;
+      alpaka::DevCpu host = alpaka::getDevByIdx(platformHost, idx);
+      std::cout << "Host[" << idx++ << "]:   " << alpaka::getName(host) << std::endl;
+    }
+  } catch (...) {
+  }
+  Platform platform;
+  std::vector<Device> alpakaDevices = alpaka::getDevs(platform);
+  idx = 0;
+  for (const auto& d : alpakaDevices) {
+    std::cout << "Device[" << idx++ << "]:   " << alpaka::getName(d) << std::endl;
+  }
+  const auto& alpakaHost = alpaka::getDevByIdx(alpaka_common::PlatformHost(), 0u);
+  CPPUNIT_ASSERT(alpakaDevices.size());
+  const auto& alpakaDevice = alpakaDevices[0];
+  Queue queue{alpakaDevice};
+
+  std::cout << "Will create torch device with type=" << torch_common::kDeviceType
+       << " and native handle=" << alpakaDevice.getNativeHandle() << std::endl;
+  torch::Device torchDevice(torch_common::kDeviceType, alpakaDevice.getNativeHandle());
 
   // Simple SOA with one bunch filled.
   const std::size_t batch_size = 35;
@@ -170,21 +192,24 @@ void testSOADataTypes::test_pose_SOA() {
   std::uniform_real_distribution<double> distrib(0, 2*M_PI);
 
   // Create and fill needed portable collections
-  PortableCollection<SoAPose, DevHost> poseCollection(batch_size, cms::alpakatools::host());
-  SoAPoseView& poseCollectionView = poseCollection.view();
+  PortableHostCollection<SoAPose> poseHostCollection(batch_size, cms::alpakatools::host());
+  PortableCollection<SoAPose, Device> poseCollection(batch_size, alpakaDevice);
+  SoAPoseView& poseHostCollectionView = poseHostCollection.view();
 
   for (size_t i = 0; i < batch_size; i++) {
-    poseCollectionView.x()[i] = 12 + i;
-    poseCollectionView.y()[i] = 2.5 * i;
-    poseCollectionView.z()[i] = 36 * i;  
-    poseCollectionView.phi()[i] = distrib(gen);  
-    poseCollectionView.psi()[i] = distrib(gen);  
-    poseCollectionView.theta()[i] = distrib(gen);  
-    poseCollectionView.t()[i] = i;  
+    poseHostCollectionView.x()[i] = 12 + i;
+    poseHostCollectionView.y()[i] = 2.5 * i;
+    poseHostCollectionView.z()[i] = 36 * i;  
+    poseHostCollectionView.phi()[i] = distrib(gen);  
+    poseHostCollectionView.psi()[i] = distrib(gen);  
+    poseHostCollectionView.theta()[i] = distrib(gen);  
+    poseHostCollectionView.t()[i] = i;  
   }
+  alpaka::memcpy(queue, poseCollection.buffer(), poseHostCollection.buffer());
 
+  // Run Converter for multiple tensors
   Mask mask(batch_size, {{torch::kDouble, torch::kInt, torch::kFloat}}, {{3, 1, 3}}, {{true, false, true}});
-  std::vector<torch::Tensor> result = Converter<SoAPose>::convert_multiple(mask, device, poseCollection.buffer().data());
+  std::vector<torch::Tensor> result = Converter<SoAPose>::convert_multiple(mask, torchDevice, poseCollection.buffer().data());
   std::cout << "Length of result vector: " << result.size() << std::endl;
   CPPUNIT_ASSERT(result.size() == 2);
 
@@ -193,21 +218,22 @@ void testSOADataTypes::test_pose_SOA() {
   }
 
   for (size_t i = 0; i < batch_size; i++) {
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.x()[i] - result[0][i][0].item<double>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.y()[i] - result[0][i][1].item<double>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.z()[i] - result[0][i][2].item<double>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.phi()[i] - result[1][i][0].item<float>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.psi()[i] - result[1][i][1].item<float>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.theta()[i] - result[1][i][2].item<float>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.x()[i] - result[0][i][0].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.y()[i] - result[0][i][1].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.z()[i] - result[0][i][2].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.phi()[i] - result[1][i][0].item<float>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.psi()[i] - result[1][i][1].item<float>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.theta()[i] - result[1][i][2].item<float>()) <= 1.0e-05);
   }
 
-  torch::Tensor tensor = Converter<SoAPose>::convert_single(batch_size, MaskElement(torch::kDouble, 3, true), device, poseCollection.buffer().data());
+  // Run Converter for single tensor
+  torch::Tensor tensor = Converter<SoAPose>::convert_single(batch_size, MaskElement(torch::kDouble, 3, true), torchDevice, poseCollection.buffer().data());
   std::cout << tensor << std::endl;
 
   for (size_t i = 0; i < batch_size; i++) {
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.x()[i] - tensor[i][0].item<double>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.y()[i] - tensor[i][1].item<double>()) <= 1.0e-05);
-    CPPUNIT_ASSERT(std::abs(poseCollectionView.z()[i] - tensor[i][2].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.x()[i] - tensor[i][0].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.y()[i] - tensor[i][1].item<double>()) <= 1.0e-05);
+    CPPUNIT_ASSERT(std::abs(poseHostCollectionView.z()[i] - tensor[i][2].item<double>()) <= 1.0e-05);
   }
 
 };
